5abe4232408e3806343694d909842848d385cd0a
Dalila G <gallegosdalila000@gmail.com>
2026-08-13 21:03:43 -0700

    project details for Aleyna added

---
+++ ecoli_walking/experiments.py
@@ -0,0 +1,16 @@
+"""
+Section 4: Population Experiements, Statistics, and Main Program
+TODO(owner: Aleyna) status - in progress
+
+Runs the simulation for many E.coli
+Oganizes and saves the results
+Provides the command line entry point for the package 
+
+Goal:
+- Run experiements for N = 10, 100, 1000 E.coli(ExpRunner)
+- Save population positions at selected iteration counts such as:
+    I = 1, 10, 50, 100, 1000
+- Run multiple random seeds when needed
+- Save & load results to file so figures do not require re-running the simulation
+- Store populations whichever form the group agreed on in Part 1(vectorized)
+"""
