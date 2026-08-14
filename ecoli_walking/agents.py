3f698b80fb4655339222fc0b5174bb7e96fa1067
Dalila G <gallegosdalila000@gmail.com>
2026-08-13 17:30:50 -0700

    project details for Emma  added

--- ecoli_walking/agents.py
+++ ecoli_walking/agents.py
@@ -4,7 +4,7 @@ TODO(owner: Dalila): status - in progress
 
 Represents an individual E.coli and controls its basic movement.
 It does NOT calculate or interpret concentration! 
-Only moves when told to by the simulaiton layer (Section 3)
+Only moves when told to by the simulaiton layer (Section 3 - Tracy)
 
 Goal: 
 - Create the Bacterium class
