5836af89387983046d703f3b80d644e45a5e75fc
Dalila G <gallegosdalila000@gmail.com>
2026-08-13 21:07:52 -0700

    project details for Nisa added

---
+++ tests/test_plotting.py
@@ -0,0 +1,9 @@
+"""
+Section 5: Plotting, Figures & Integration
+TODO(owner: Nisa) status - in progress
+
+Goal:
+-  Perform final integration testing — confirm that all parts of the
+    package work together end to end
+
+"""
