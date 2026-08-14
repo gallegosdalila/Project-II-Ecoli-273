6273e531c4ade947990585026776b06f8cfb1196
Dalila G <gallegosdalila000@gmail.com>
2026-08-14 10:35:37 -0700

    config updated

--- ecoli_walking/__init__.py
+++ ecoli_walking/__init__.py
@@ -1,7 +1,7 @@
 #This file turns the folder into a package
 
 from .agents import Bacterium 
-from .config import def_stepLength, def_dt, def_seed
+from .config import default_stepLength, default_dt, default_seed
 
-__all__ = ["Bacterium, def_stepLength, def_dt, deef_seed"]
+__all__ = ["Bacterium, default_stepLength, default_dt, default_seed"]
 
--- ecoli_walking/config.py
+++ ecoli_walking/config.py
@@ -4,14 +4,31 @@ TODO(owner: Dalila): status - in progress
 
 Goal:
 - Store default simulation parameters here (step length, dt, etc)
-  so other sections import from a single source of truth instead of HARDCODING!!!
+  so other sections import from a single source instead of HARDCODING!!!
 
 """
 
-def_stepLength = 1.0 #run step, arbitrary value
-def_dt = 0.01 #simulation timestep (s)
+default_stepLength = 1.0 #run step, arbitrary value
+default_dt = 0.01 #simulation timestep (s)
 def_tumbles_perCycle = 4 #for section 3(Emma) gradient-check
-def_seed = 42 #reproducable steps/exp's
+
+#Numpy random seed generator
+#can change seed for different results later on
+default_seed = 42 #reproducable steps/exp's
+rng = np.random.default_rng(default_seed) #local random generator instance
+
+#E.coli movement
+speed = 20 #(um/s) units , average speed of E.coli swimming
+
+#Model constants
+initial = [0,0] #cells start at the origin
+ligand_center = [1000,1000] #position for highest concentration #can change later
+center_exp, start_exp = 8,2 #exponent for concetration at [1000, 1000] and [0,0]
+
+origin_to_center = 0 #distance from start to center initialized here
+saturation_conc = 10**8 #change based on other sections?
+
+
 
 
 
