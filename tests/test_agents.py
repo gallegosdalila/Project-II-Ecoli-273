cb2f0a24bf5a968d404b4363ea1de768aa7511b2
Dalila G <gallegosdalila000@gmail.com>
2026-08-14 21:55:38 -0700

    recent updates to test agents

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -15,7 +15,7 @@ import numpy as np
 #import our Bacterium class made in agents 
 from ecoli_walking.agents import Bacterium 
 
-def test_direction_l():
+def test_direction_length():
     rng = np.random.default_rng(0) #0 is the starting point for randomness
     #starts at origin [0,0] then pick the angle with rng
     #since no constructor is passed, the helper method created earlier is called
@@ -26,8 +26,25 @@ def test_direction_l():
     #np.linalg.norm will compute the magnitude(length) of the vector 
     #np.isclose will wheck if the normalized direction is close to 1.0 (estimation)
     #assert will give an error if the test/check failed to be close to 1.0
-    #check line saying: 
+    #what this checks: take the ecoli current direction vector, measure length & give error if not approx. 1 
     assert np.isclose(np.linalg.norm(b.direction), 1.0)
+
+def test_tumble_direction():
+    rng = np.random.default_rng(1)
+    b = Bacterium(position = [0,0], rng = rng)
+    #call the tumble method defined in the Bacterium class in agents.py onto b
+    #wherever it says 'self' in the method, you apply b in its place
+    b.tumble()
+    assert np.isclose(np.linalg.norm(b.direction), 1.0)
+
+def test_runStep_length():
+    b = Bacterium(position = [0,0])
+    start = b = b.position.copy()
+    b.run(direction = [0,0], length = 2.5)
+    assert np.isclose(np.linalg.norm(b.position - start), 2.5)
+
+
+
 
 
 
