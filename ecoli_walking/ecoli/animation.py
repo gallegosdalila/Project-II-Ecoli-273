e98ead8882f3c3ba237a606a93da7e1046972116
tracy-bits <3123313784+tracy-bits@users.noreply.github.com>
2026-08-16 23:23:21 -0700

    updated typo

--- ecoli_walking/ecoli/animation.py
+++ ecoli_walking/ecoli/animation.py
@@ -48,7 +48,7 @@ def animate_walk( result: SimulationResult, save_path: str, field: Optional[Conc
     dots = ax.scatter(positions[:, 0, 0], positions[:, 0, 1], c="white", edgecolors="black", zorder=5,)
 
     label = ax.text( 0.02, 0.98, "", transform=ax.transAxes, va="top",
-        bbox=dict( facecolor="white", alpha=0.8, edgecolor="none",),x)
+        bbox=dict( facecolor="white", alpha=0.8, edgecolor="none",))
 
     ax.set_title(f"E. coli chemotaxis (N shown = {n_show})")
 
